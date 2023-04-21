4b41b498e2dc57c896830c06716a9d8e6689741a
2CrAzYDrAgOn2 <102369349+2CrAzYDrAgOn2@users.noreply.github.com>
2023-04-21 22:30:16 +0300

    Класс Person

---
+++ untitled2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        Person person2 = new Person("John Smith", 30);
+
+        person1.move();
+        person1.talk();
+
+        person2.move();
+        person2.talk();
+    }
+}
---
+++ untitled2/src/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+    private String fullName;
+    private int age;
+
+    public Person() {
+        fullName = "Unknown";
+        age = 0;
+    }
+
+    public Person(String fullName, int age) {
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    public void move() {
+        System.out.println(fullName + " двигается.");
+    }
+
+    public void talk() {
+        System.out.println(fullName + " говорит.");
+    }
+}
